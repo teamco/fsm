444d55d2e42e333a9b746823da7f01778e5ea1b4
Volodymyr Tkach <teamco@gmail.com>
2024-09-15 23:20:50 +0300

    fix e2e tests

--- __tests__/e2e/specs/landing/landing.spec.js
+++ __tests__/e2e/specs/landing/landing.spec.js
@@ -3,22 +3,49 @@ const { test, expect } = require('@playwright/test');
 const { SELECTORS } = require('../../utils/selectots');
 const { attachThumb } = require('../../utils/helpers');
 
-test.describe('Landing suite', () => {
-  const baseUrl = `http://localhost:3000`;
+const {
+  UI_SERVER
+} = process.env;
 
-  test(`Navigate to: ${baseUrl}`, async ({ page }) => {
+test.describe('Landing suite', () => {
+  const baseUrl = `${UI_SERVER}`;
+
+  test(`Navigate to: ${baseUrl}`, async ({ page }, testInfo) => {
     await page.goto(baseUrl, { waitUntil: 'domcontentloaded' });
     await page.waitForURL(baseUrl);
 
     await expect(page).toHaveTitle(/React App/);
     await expect(page.locator(SELECTORS.landing)).toBeVisible();
+    await attachThumb({ locator: page, testInfo, name: 'Landing' });
+
+    await expect(page.locator(SELECTORS.redLight)).toBeVisible();
+    await expect(page.locator(SELECTORS.blinkLight)).not.toBeVisible();
+    await expect(page.locator(SELECTORS.yellowLight)).not.toBeVisible();
+    await expect(page.locator(SELECTORS.greenLight)).not.toBeVisible();
+    
+    expect(page.locator(SELECTORS.counter).filter({ hasText: "Counter: 0" })).toBeTruthy();
+    expect(page.locator('button').filter({ hasText: "Start" })).toBeTruthy();
+    
+    await page.close();
   });
 
-  // test(`Handle 404: ${_404}`, async ({ page }, testInfo) => {
-  //   await page.goto(`${_404}`, { waitUntil: 'domcontentloaded' });
-  //   await page.waitForURL(`${_404}`);
+  test(`Check start button behavior: Start/Clear`, async ({ page }, testInfo) => {
+    await page.goto(baseUrl, { waitUntil: 'domcontentloaded' });
+    await page.waitForURL(baseUrl);
 
-  //   await expect(page.locator(SELECTORS.page404)).toBeVisible();
-  //   await attachThumb({ locator: page, testInfo, name: 'page404' });
-  // });
+    await expect(page.getByRole('button', { name: /Start/i })).toBeVisible();
+    await expect(page.getByRole('button', { name: /Clear/i })).not.toBeVisible();
+
+    await page.getByRole('button', { name: /Start/i }).click();
+    
+    await expect(page.getByRole('button', { name: /Start/i })).not.toBeVisible();
+    await expect(page.getByRole('button', { name: /Clear/i })).toBeVisible();
+
+    await page.getByRole('button', { name: /Clear/i }).click();
+    
+    await expect(page.getByRole('button', { name: /Start/i })).toBeVisible();
+    await expect(page.getByRole('button', { name: /Clear/i })).not.toBeVisible();
+
+    await page.close();
+  });
 });
--- __tests__/e2e/utils/selectots.js
+++ __tests__/e2e/utils/selectots.js
@@ -1,3 +1,8 @@
 export const SELECTORS = {
-  landing: '#root > div[class^="app"]'
+  landing: '#root > div[class^="app"]',
+  redLight: 'div[class*="red"] span:not([class*=disabled])',
+  yellowLight: 'div[class*="yellow"] span:not([class*=disabled])',
+  blinkLight: 'div[class*="blink"] span:not([class*=disabled])',
+  greenLight: 'div[class*="green"] span:not([class*=disabled])',
+  counter: 'div[class*="counter"]',
 };
